6d16023189e3adfcbf4913c18cf922e869915376
驰荣 张 <931805085@163.com>
2016-12-05 00:09:29 +0800

    修改了文件编码

--- StudentManageSystem/ZCHtest.c
+++ StudentManageSystem/ZCHtest.c
@@ -6,8 +6,8 @@ int main()
 	Chart chart = {0};
 	char list[2][32] = { "±àºÅ","ĞÕÃû" };
 	int list2[2] = { 5,5 };
-	printf("%d\n",ReadFromFile("data.ini", &chart));
-	//CreateNewUnit(&chart, 1, list, list2);
+	//printf("%d\n",ReadFromFile("data.ini", &chart));
+	CreateNewUnit(&chart, 1, list, list2);
 	//printf("%s  %s  %s\n", chart.Chart[0][0], chart.Chart[0][1], chart.Chart[0][1]);
 	
 	system("pause");
